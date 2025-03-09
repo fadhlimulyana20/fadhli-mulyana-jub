ac3ce1476e44a37746dea32de6995fa9da68f022
Fadhli Mulyana <baba.fadhli@gmail.com>
2025-03-09 09:06:56 +0700

    feat: add product list management page

---
+++ frontend/pages/management/product/index.tsx
@@ -0,0 +1,72 @@
+import TableKey from "@/components/molecules/table"
+import { MainNavbar } from "@/components/organisms/navbar/main"
+import { Product, RemoteGetProductList } from "@/models/product"
+import { useCallback, useEffect, useState } from "react"
+
+export default function ProductManagementIndexPage() {
+    const [products, setProducts] = useState<Array<Product>>([])
+
+    const tableOptions = [
+        {
+            name: 'ID',
+            accessor: 'id',
+        },
+        {
+            name: 'Name',
+            accessor: 'title',
+        },
+        {
+            name: 'Stock',
+            accessor: 'stock',
+        },
+        {
+            name: 'Price',
+            accessor: 'price',
+        },
+    ]
+
+    const tableActions = [
+        {
+            name: 'Edit',
+            action: (id: any) => {
+                // navigate(`/user/edit/${id}`)
+            }
+        },
+        {
+            name: 'Hapus',
+            action: (id: any) => {
+                // setUserIDSelected(id)
+                // setModalDeleteOpen(true)
+            }
+        }
+    ]
+
+
+    const fetchProduct = useCallback(async () => {
+        try {
+            const res = await RemoteGetProductList()
+            if (typeof res !== "undefined") {
+                setProducts(res)
+            }
+        } catch (e: any) {
+            console.log(e)
+        }
+    }, [])
+
+    useEffect(() => {
+        fetchProduct()
+    }, [])
+
+    return (
+        <>
+            <MainNavbar />
+            <div className="container xl:max-w-screen-xl lg:maw-sccreen-lg lg:px-8 px-4 mx-auto py-10">
+                <TableKey
+                    data={products}
+                    actions={tableActions}
+                    options={tableOptions}
+                />         
+            </div>
+        </>
+    )
+}
