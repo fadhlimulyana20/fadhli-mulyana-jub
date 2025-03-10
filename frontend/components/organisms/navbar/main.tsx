66ae6344199db2b3a1f6906755a6d04b308c09a2
Fadhli Mulyana <baba.fadhli@gmail.com>
2025-03-10 13:01:02 +0700

    feat: add product filter on storefront

--- frontend/components/organisms/navbar/main.tsx
+++ frontend/components/organisms/navbar/main.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 import { useTheme } from "next-themes"
 import Link from "next/link"
+import { useState } from "react"
 
 const links = [
     {
@@ -19,6 +21,7 @@ const links = [
 
 export function MainNavbar() {
     const { theme, setTheme } = useTheme()
+    const [hide, setHide] = useState(true)
 
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-800">
@@ -26,21 +29,25 @@ export function MainNavbar() {
                 <Link href="/">
                     <p className="text-2xl font-semibold">Store <span className="font-bold">Manager</span></p>
                 </Link>
-                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                <button onClick={() => setHide(!hide)} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                     </svg>
                 </button>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 items-center">
+                <div className={cn("w-full md:block md:w-auto", hide ? "hidden" : '')} id="navbar-default">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 md:items-center md:space-y-0 space-y-4">
                         {links.map((o, i) => (
                             <li key={i}>
-                                <Link href={o.href} className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">{ o.label }</Link>
+                                <Link href={o.href} className="w-full">
+                                    <span className="block py-2 px-3 text-gray-800 hover:text-blue-500 rounded-sm md:bg-transparent md:p-0 dark:text-white md:dark:text-blue-500">
+                                        {o.label}
+                                    </span>
+                                </Link>
                             </li>
                         ))}
                         <li>
-                            <Button 
+                            <Button
                                 onClick={() => {
                                     if (theme === "light") {
                                         setTheme("dark")
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import ProductCard from "@/components/molecules/card/productCard";
-import { useCallback, useEffect, useState } from "react";
-import { IProductFilter, Product, RemoteGetProductList } from "@/models/product";
+import { SyntheticEvent, useCallback, useEffect, useRef, useState } from "react";
+import { IProductFilter, Product, RemoteGetProductCategories, RemoteGetProductList } from "@/models/product";
 import { MainNavbar } from "@/components/organisms/navbar/main";
 import Header from "@/components/atoms/head";
+import { useRouter } from "next/router";
+import { IMeta } from "@/models/responseHttp";
+import { PaginationPure } from "@/components/molecules/pagination";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,48 +25,153 @@ const geistMono = Geist_Mono({
 
 
 export default function Home() {
+  const router = useRouter()
   const [products, setProducts] = useState<Array<Product>>([])
+  const [categories, setCategories] = useState<Array<string>>([])
+  const [isFetching, setIsFetching] = useState(false)
+  const searchRef = useRef<HTMLInputElement>(null)
+  const [meta, setMeta] = useState<IMeta>({
+    count: 0,
+    limit: 10,
+    page: 1
+  })
 
   const fetchProduct = useCallback(async (param: IProductFilter) => {
+    setIsFetching(true)
     try {
       const res = await RemoteGetProductList(param)
       if (typeof res !== "undefined") {
         setProducts(res.data || [])
+        if (typeof res.meta !== "undefined") {
+          setMeta(res.meta)
+        }
+      }
+    } catch (e: any) {
+      console.log(e)
+    } finally {
+      setIsFetching(false)
+    }
+  }, [])
+
+  const fetchCategories = useCallback(async () => {
+    try {
+      const res = await RemoteGetProductCategories()
+      console.log(res?.data)
+      if (typeof res?.data !== "undefined") {
+        setCategories([...res.data])
       }
     } catch (e: any) {
       console.log(e)
     }
   }, [])
 
-  useEffect(() => {
-    const param: IProductFilter = {
+  const handleChangePage = (page: number) => {
+    router.push({ query: { ...router.query, page: page } }, undefined, { shallow: true })
+  }
 
+  const handleSubmitSearch = (e: SyntheticEvent<HTMLFormElement, SubmitEvent>) => {
+    e.preventDefault()
+    router.push({ query: { ...router.query, search: encodeURIComponent(searchRef.current?.value || '') } }, undefined, { shallow: true })
+  }
+
+  const handleCategorySelect = (value: string) => {
+    router.push({ query: { ...router.query, category: encodeURIComponent(value) } }, undefined, { shallow: true })
+  }
+
+  useEffect(() => {
+    if (router.isReady) {
+      const param: IProductFilter = {}
+
+      const { page, search, category } = router.query
+
+      if (typeof page !== "undefined") {
+        param.page = Number(page)
+      } else {
+        param.page = 1
+      }
+
+      if (typeof search !== "undefined") {
+        param.search = String(search)
+      } else {
+        param.search = undefined
+      }
+
+      if (typeof category !== "undefined") {
+        if (String(category) === "all") {
+          param.category = undefined
+        } else {
+          param.category = String(category)
+        }
+      } else {
+        param.category = undefined
+      }
+
+
+      fetchProduct(param)
     }
-    fetchProduct(param)
+  }, [router])
+
+  useEffect(() => {
+    fetchCategories()
   }, [])
 
   return (
     <>
-      <Header 
+      <Header
         title="Store"
         description="Store"
       />
       <MainNavbar />
       <div className="container px-8 max-w-screen-xl mx-auto py-10">
-        <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-x-6 gap-y-8">
-          {products.map((o, i) => (
-            <div
-              key={i}
-            >
-              <ProductCard
-                name={o.title}
-                category={o.category}
-                href={`/product/${o.id}`}
-                image={o.image}
-                price={String(o.price)}
-              />
+        <div className="mb-4 flex gap-4 items-center">
+          <form onSubmit={handleSubmitSearch}>
+            <div className="flex gap-1">
+              <Input ref={searchRef} placeholder="Search Product" />
+              <Button type="submit"><Search /></Button>
             </div>
-          ))}
+          </form>
+          <Select onValueChange={(value) => handleCategorySelect(value)}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select Category" defaultValue={'Select Category'} />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">All</SelectItem>
+              {categories.map((o, i) => (
+                <SelectItem key={i} value={o}>{o}</SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="space-y-10">
+          <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-x-6 gap-y-8">
+            {isFetching ? (
+              [...Array(5)].map((o) => (
+                <div key={o} className="bg-gray-200 dark:bg-gray-700 rounded-lg animate-pulse w-full h-54 rounded-lg" />
+              ))
+            ) : (
+              products.map((o, i) => (
+                <div
+                  key={i}
+                >
+                  <ProductCard
+                    name={o.title}
+                    category={o.category}
+                    href={`/product/${o.id}`}
+                    image={o.image}
+                    price={String(o.price)}
+                  />
+                </div>
+              ))
+            )}
+          </div>
+          <PaginationPure
+            pages={Math.ceil(meta.count / meta.limit)}
+            activePage={meta.page}
+            onChangePage={(arg: number) => {
+              handleChangePage(arg)
+            }}
+            key={meta.page}
+          />
         </div>
       </div>
     </>
